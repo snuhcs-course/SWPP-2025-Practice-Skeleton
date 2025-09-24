cc81b80336ccf2ae8c2106395573ac02abd8cbe5
Bona Park <jaeyeon.park@hcs.snu.ac.kr>
2025-09-24 22:55:35 +0900

    Initial commit for Tutorial 4

---
+++ LocationDemo/app/src/main/java/com/example/locationdemo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.locationdemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.os.Bundle
+import android.os.Looper
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationTextView: TextView
+    private var mMap: GoogleMap? = null
+    private val pathPoints = mutableListOf<LatLng>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
